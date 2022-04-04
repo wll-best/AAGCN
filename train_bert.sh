447c3677db47f816a44c817f41f6189a19022b84
wll <62608056+wll-best@users.noreply.github.com>
2022-04-04 14:43:57 +0800

    Add files via upload

---
+++ train_bert.sh
@@ -0,0 +1,3 @@
+python train_bert.py --model aagcn_bert --dataset rest15_sen --device cuda:0
+python train_bert.py --model aagcn_bert --dataset rest16_sen --device cuda:0
+python train_bert.py --model aagcn_bert --dataset 14_sen --device cuda:0
